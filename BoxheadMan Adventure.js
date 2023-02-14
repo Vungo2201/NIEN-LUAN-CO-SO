8cca78669ce238805c92d72738693c976d42b714
Vungo2201 <vu22012002@gmail.com>
2023-02-14 07:49:34 +0700

    Tich hop class HeadboxMan, tao gravati

--- BoxheadMan Adventure.js
+++ BoxheadMan Adventure.js
@@ -1,24 +1,68 @@
- import { Boxheadman } from './Boxheadman'
 window.addEventListener('load', function(){
     const canvas = document.getElementById("canvasForGame");
     const ctx = canvas.getContext("2d");
     canvas.width = 700;
     canvas.height = 500;
-    
-    class gameScreen {
-        constructor(width,height){
-            this.width = width;
-            this.height = height;
-            this.Boxheadman = new Boxheadman(this);
+    const gravity = 2;
+
+    class BoxheadMan {
+        constructor(){
+            this.width = 100;
+            this.height = 100;
+            this.x=0;
+            this.y=280;
+            this.speedX = 5;
+            this.speedY = 5;
+            this.Image = document.getElementById('player');
         }
-        updata(){
-            
+        update(){
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.y += gravity;
+            this.draw();
         }
         draw(){
-
+             //phương thức vẽ hình ảnh của canvas ctx.drawImage(player,x start render of photo,y start render of photo,width photo,height photo, x start render of canvas, y start render of canvas,canvas width render,canvas height render);
+            ctx.drawImage(this.Image,0,0,200,200,this.x,this.y,this.width,this.height)
         }
     }
-});
+    const BoxHeadMan = new BoxheadMan();
+    BoxHeadMan.update();
+
+    function playerAnimate(){
+        BoxHeadMan.update();
+        requestAnimationFrame(playerAnimate);
+    }
+    playerAnimate();
+ }
+)
+//     class gameScreen {
+//         constructor(width,height){
+//             this.width = width;
+//             this.height = height;
+//             this.Boxheadman = new BoxheadMan(this);
+//             this.key = "ArrowRight";
+//         }
+//         update(key){
+//             if(key ===  "ArrowRight")this.BoxheadMan.update(5,0);
+//             else if(key ===  "ArrowLeft")this.BoxheadMan.update(-5,0);
+//             else if(key ===  "ArrowUp")this.BoxheadMan.update(-5,0);
+//             else if(key ===  "ArrowRight")this.BoxheadMan.update(-5,0);
+//         }
+//         draw(Context){
+//             this.Boxheadman.draw(Context);
+//         }
+//     }
+//     const gameScreen1 = new gameScreen(canvas.width,canvas.height);
+//     console.log(gameScreen1);
+//     function playerAnimate(){
+//         ctx.clearRect(0,0,canvas.width,canvas.height)
+//         gameScreen1.update(gameScreen1.key);
+//         gameScreen1.draw(ctx);
+//         requestAnimationFrame(playerAnimate);
+//     }
+//     playerAnimate();
+// });
 
 let gameFrame = 0;
 const controlFrame = 2;
@@ -30,7 +74,6 @@ var playerRunImageNumber = 1;
 
 function runAnimation(){
     playerRun.src = '/BoxHeadMan sprites/run/run ('+ playerRunImageNumber +').png'
-     //ctx.drawImage(player,x start render of photo,y start render of photo,width photo,height photo, x start render of canvas, y start render of canvas,canvas width render,canvas height render);
     ctx.drawImage(playerRun,0,0,200,200,player.Xplayer,player.Yplayer,100,100);
     if(gameFrame%controlFrame==0){
         if (playerRunImageNumber < 21) playerRunImageNumber++;
@@ -101,4 +144,3 @@ function loopBackground(){
     X2ofBackground -= controlFrame;
     requestAnimationFrame(loopBackground);
 }
-

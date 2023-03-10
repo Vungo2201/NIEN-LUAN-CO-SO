7e96519374509d2d5c65952eeb1414760bb1650d
Vungo2201 <vu22012002@gmail.com>
2023-03-11 03:28:36 +0700

    Hoan thien hoat anh, chinh sua cac anh

--- BoxheadMan Adventure.js
+++ BoxheadMan Adventure.js
@@ -6,29 +6,32 @@ canvas.width = 1300;
 canvas.height = 1000;
 
 const graviti = 1;
+var run = 1;
+var jump = 1;
+var live = 0;
+var game = true;
 
 class sprites {
     constructor(){
-        this.Image_position = {
+        this.Position = {
             X: 0,
             Y: 0,
             X_canvas: 0,
             Y_canvas: 0,
         }
-        this.Image_shape = {
-            with: 0,
+        this.Shape = {
+            width: 0,
             height: 0,
             width_canvas: 0,
             height_canvas: 0
         }
 
         this.image = new Image();
-        this.image.src = null;
     }
 
     draw(){
         if(this.image.src != null)
-            return ctx.drawImage(this.image,this.Image_position.X,this.Image_position.Y,this.Image_shape.with,this.Image_shape.height,this.Image_position.X_canvas,this.Image_position.Y_canvas,this.Image_shape.width_canvas,this.Image_shape.height_canvas)
+            return ctx.drawImage(this.image,this.Position.X,this.Position.Y,this.Shape.width,this.Shape.height,this.Position.X_canvas,this.Position.Y_canvas,this.Shape.width_canvas,this.Shape.height_canvas);
     }
 
     update(){
@@ -36,76 +39,97 @@ class sprites {
     }
 }
 
-class BoxheadMan {
+class BoxheadMan extends sprites{
     constructor(){
-        this.Position = {
-            X: 0,
-            Y: 0
-        }
-        this.shape = {
-            width: 150,
-            height: 150
+        super();
+        this.Shape = {
+            width: 175,
+            height: 175,
+            width_canvas: 200,
+            height_canvas: 200
         }
         this.move = {
             speedX: 0,
             speedY: 0
         }
     }
-    
-    draw(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.Position.X,this.Position.Y,this.shape.width,this.shape.height);
-        //method draw Image of canvas ctx.drawImage(photo,x start render of photo,y start render of photo,width photo,height photo, x start render of canvas, y start render of canvas,canvas width render,canvas height render);
-        //ctx.drawImage(this.Image,0,0,200,200,this.x,this.y,this.width,this.height)
-    }
-
     update() {
         this.draw();
         
-        this.Position.X += this.move.speedX;
-        this.Position.Y += this.move.speedY;
+        this.Position.X_canvas += this.move.speedX;
+        this.Position.Y_canvas += this.move.speedY;
 
-        if(this.Position.Y + this.shape.height + this.move.speedY < canvas.height)
+        if(this.Position.Y_canvas + this.Shape.height + this.move.speedY < canvas.height - 250)
             this.move.speedY += graviti;
         else this.move.speedY = 0;
 
-
+        if(this.Position.X_canvas  - this.Shape.width_canvas > canvas.height)
+            this.Position.X_canvas = 0;
+        else if(this.Position.X_canvas + this.Shape.width_canvas < 0) this.Position.X_canvas = canvas.width-this.Shape.width_canvas;
     }
 }
 
 var keysInput = {
     left_Key : false,
-    right_Key : false
+    right_Key : false,
+    up_Key : false
 }
 const BoxHeadMan = new BoxheadMan();
+BoxHeadMan.image.src = './BoxHeadMan sprites/run/run (2).png';
 const backgroundGame = new sprites();
+
+backgroundGame.Shape.width = 600;
+backgroundGame.Shape.height = 340;
+backgroundGame.Shape.width_canvas = canvas.width;
+backgroundGame.Shape.height_canvas = canvas.height;
+backgroundGame.image.src = '/layer/backgroundGame.png';
 
 function animate(){
     window.requestAnimationFrame(animate);
     ctx.fillStyle = 'white';
     ctx.fillRect(0,0,canvas.width,canvas.height);
 
-    BoxHeadMan.update();
+    ctx.save();
+    backgroundGame.update();
+    ctx.restore();
 
+    BoxHeadMan.update();
     BoxHeadMan.move.speedX = 0;
 
-    if(keysInput.right_Key) BoxHeadMan.move.speedX = 2.5;
-    else if(keysInput.left_Key)  BoxHeadMan.move.speedX = -2.5;
+    if(keysInput.right_Key){
+        BoxHeadMan.image.src = '/BoxHeadMan sprites/run/run ('+ run +').png';
+        if(run < 21) run++;
+        else run = 1;
+        BoxHeadMan.move.speedX = 4;
+    }
+    else if(keysInput.left_Key){
+        BoxHeadMan.image.src = '/BoxHeadMan sprites/run/runleft ('+ run +').png';
+        if(run < 20) run++;
+        else run = 1;
+        BoxHeadMan.move.speedX = -4;
+    } else if(keysInput.up_Key) BoxHeadMan.move.speedY = -18;
+
+    if(live == 0){
+        for(i = 1;i < 22;i++) BoxHeadMan.image.src = '/BoxHeadMan sprites/KO/KO ('+ i +').png';
+        // game = true;
+    }
 }
 
-animate()
+// while(game == true){
+    animate()
+// }
+
 
 addEventListener('keydown',(Event) => {
     switch(Event.key) {
         case 'd':
         case 'ArrowRight':
         keysInput.right_Key = true;
-        console.log(keysInput)
         break;
 
         case "ArrowUp":
         case "w":
-        BoxHeadMan.move.speedY = -25;
+        keysInput.up_Key = true;
         break;
 
         case "a":
@@ -121,6 +145,11 @@ addEventListener('keyup',(Event) => {
         keysInput.right_Key = false;
         break;
 
+        case "ArrowUp":
+        case "w":
+        keysInput.up_Key = false;
+        break;
+        
         case "a":
         case "ArrowLeft":
         keysInput.left_Key = false;

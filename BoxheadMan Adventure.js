1c7cd1d50ac41c483c49372752f6aa51dbe20bd1
Vungo2201 <vu22012002@gmail.com>
2023-03-11 23:20:30 +0700

    tao chuyen dong nen,tinh diem,tinh thoi gian

--- BoxheadMan Adventure.js
+++ BoxheadMan Adventure.js
@@ -8,8 +8,11 @@ canvas.height = 1000;
 const graviti = 1;
 var run = 1;
 var jump = 1;
-var live = 0;
-var game = true;
+var liveNumber = 3;
+//control star game,end game
+var gamestate = 1;
+var Timer = 0;
+var score = 0 ;
 
 class sprites {
     constructor(){
@@ -72,55 +75,10 @@ class BoxheadMan extends sprites{
 var keysInput = {
     left_Key : false,
     right_Key : false,
-    up_Key : false
 }
-const BoxHeadMan = new BoxheadMan();
-BoxHeadMan.image.src = './BoxHeadMan sprites/run/run (2).png';
-const backgroundGame = new sprites();
-
-backgroundGame.Shape.width = 600;
-backgroundGame.Shape.height = 340;
-backgroundGame.Shape.width_canvas = canvas.width;
-backgroundGame.Shape.height_canvas = canvas.height;
-backgroundGame.image.src = '/layer/backgroundGame.png';
-
-function animate(){
-    window.requestAnimationFrame(animate);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-
-    ctx.save();
-    backgroundGame.update();
-    ctx.restore();
-
-    BoxHeadMan.update();
-    BoxHeadMan.move.speedX = 0;
-
-    if(keysInput.right_Key){
-        BoxHeadMan.image.src = '/BoxHeadMan sprites/run/run ('+ run +').png';
-        if(run < 21) run++;
-        else run = 1;
-        BoxHeadMan.move.speedX = 4;
-    }
-    else if(keysInput.left_Key){
-        BoxHeadMan.image.src = '/BoxHeadMan sprites/run/runleft ('+ run +').png';
-        if(run < 20) run++;
-        else run = 1;
-        BoxHeadMan.move.speedX = -4;
-    } else if(keysInput.up_Key) BoxHeadMan.move.speedY = -18;
-
-    if(live == 0){
-        for(i = 1;i < 22;i++) BoxHeadMan.image.src = '/BoxHeadMan sprites/KO/KO ('+ i +').png';
-        // game = true;
-    }
-}
-
-// while(game == true){
-    animate()
-// }
-
 
 addEventListener('keydown',(Event) => {
+    console.log(Event.key)
     switch(Event.key) {
         case 'd':
         case 'ArrowRight':
@@ -129,7 +87,7 @@ addEventListener('keydown',(Event) => {
 
         case "ArrowUp":
         case "w":
-        keysInput.up_Key = true;
+        BoxHeadMan.move.speedY = -28;
         break;
 
         case "a":
@@ -144,11 +102,6 @@ addEventListener('keyup',(Event) => {
         case 'ArrowRight':
         keysInput.right_Key = false;
         break;
-
-        case "ArrowUp":
-        case "w":
-        keysInput.up_Key = false;
-        break;
         
         case "a":
         case "ArrowLeft":
@@ -156,4 +109,78 @@ addEventListener('keyup',(Event) => {
         break;
     }
 });
+
+const BoxHeadMan = new BoxheadMan();
+BoxHeadMan.image.src = '/BoxHeadMan sprites/run/run (19).png';
+
+var live = new sprites();
+live.image.src = '/layer/heart.png';
+
+var backgroundGame = new sprites();
+var backgroundGame2 = new sprites();
+
+backgroundGame2.Shape.width=backgroundGame.Shape.width = 600;
+backgroundGame2.Shape.height=backgroundGame.Shape.height = 340;
+backgroundGame2.Shape.width_canvas=backgroundGame.Shape.width_canvas = canvas.width;
+backgroundGame2.Shape.height_canvas=backgroundGame.Shape.height_canvas = canvas.height;
+backgroundGame2.image.src = backgroundGame.image.src = '/layer/backgroundGame.png';
+backgroundGame2.Position.X_canvas = 1300;
+
+function animate(){
+    if(gamestate==1){
+        Timer++;
+        window.requestAnimationFrame(animate);
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+
+        ctx.save();
+        backgroundGame.update();
+        backgroundGame2.update();
+        ctx.restore();
+      
+        for(let i = 0;i < liveNumber; i++){
+            ctx.drawImage(live.image,0,0,80,80,30+75*i,30,80,80);
+        }
+
+        ctx.font = "40px Arial black";
+        ctx.fillText("Score: " + (score*0.05).toFixed(0) ,canvas.width-350,80);
+        ctx.fillText("Time: " + (Timer*0.028).toFixed(0) ,canvas.width-350,150 )
+
+        BoxHeadMan.update();
+        BoxHeadMan.move.speedX = 0;
+
+        if(keysInput.right_Key){
+            BoxHeadMan.image.src = '/BoxHeadMan sprites/run/run ('+ run +').png';
+            if(run < 21) run++;
+            else run = 1;
+            BoxHeadMan.move.speedX = 4;
+            backgroundGame.Position.X_canvas--;
+            backgroundGame2.Position.X_canvas--;
+            score++;
+        }
+        else if(keysInput.left_Key){
+            BoxHeadMan.image.src = '/BoxHeadMan sprites/run/runleft ('+ run +').png';
+            if(run < 20) run++;
+            else run = 1;
+            BoxHeadMan.move.speedX = -4;
+            backgroundGame.Position.X_canvas++;
+            backgroundGame2.Position.X_canvas++;
+            score--;
+        } 
+
+        if(backgroundGame.Position.X_canvas<-1300) backgroundGame.Position.X_canvas = 1299;
+        if(backgroundGame2.Position.X_canvas<-1300) backgroundGame2.Position.X_canvas = 1299;
+        if(backgroundGame.Position.X_canvas>1300) backgroundGame.Position.X_canvas = -1299;
+        if(backgroundGame2.Position.X_canvas>1300) backgroundGame2.Position.X_canvas = -1299;
+
+        if(score<0) score=0;
+
+        if(liveNumber == 0){
+            for(i = 1;i < 22;i++) BoxHeadMan.image.src = '/BoxHeadMan sprites/KO/KO ('+ i +').png';
+            // gamestate = 2;
+        }
+    }
+}
+
+animate()
 
